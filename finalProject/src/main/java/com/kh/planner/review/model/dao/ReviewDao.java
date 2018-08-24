235c99663436174d4887c3fc11c7288cd6153215
choiwon <choiwon@LAPTOP-QAQ7GR1E>
2018-08-24 16:08:01 +0900

    리뷰

---
+++ finalProject/src/main/java/com/kh/planner/review/model/dao/ReviewDao.java
@@ -0,0 +1,32 @@
+package com.kh.planner.review.model.dao;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.planner.review.model.vo.Review;
+
+@Repository
+public class ReviewDao {
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	public int insertReview(Review review) {
+		return sqlSession.insert("ReviewMapper.insertReview", review);
+	}
+
+	public List<Review> selectReviewAll(Review review) {
+		return sqlSession.selectList("ReviewMapper.selectReviewAll", review);
+	}
+
+	public int deleteReview(Review review) {
+		return sqlSession.delete("ReviewMapper.deleteReview", review);
+	}
+
+	public int updateReview(Review review) {
+		return sqlSession.update("ReviewMapper.updateReview", review);
+	}
+
+}
---
+++ finalProject/src/main/java/com/kh/planner/review/model/service/ReviewService.java
@@ -0,0 +1,33 @@
+package com.kh.planner.review.model.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.planner.review.model.dao.ReviewDao;
+import com.kh.planner.review.model.vo.Review;
+
+@Service
+public class ReviewService {
+
+	@Autowired
+	ReviewDao dao;
+	
+	public int insertReview(Review review) {
+		return dao.insertReview(review);
+	}
+
+	public List<Review> selectReviewAll(Review review) {
+		return dao.selectReviewAll(review);
+	}
+
+	public int deleteReview(Review review) {
+		return dao.deleteReview(review);
+	}
+
+	public int updateReview(Review review) {
+		return dao.updateReview(review);
+	}
+
+}
