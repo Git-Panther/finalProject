8fce9b78e86d16b63764a75681c36e9e90a6952e
TaeYeong <TaeYeong@DESKTOP-IJCD0SF>
2018-08-28 13:31:49 +0900

    fdsf

--- finalProject/src/main/java/com/kh/planner/plan/model/service/PlanService.java
+++ finalProject/src/main/java/com/kh/planner/plan/model/service/PlanService.java
@@ -1,9 +1,13 @@
 package com.kh.planner.plan.model.service;
 
+import com.kh.planner.member.model.vo.Member;
 import com.kh.planner.plan.model.dao.PlanDao;
+import com.kh.planner.plan.model.vo.Plan;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -18,4 +22,17 @@ public class PlanService {
     public List<Map> selectFavoriteHotel(Map<String, Integer> params) {
         return dao.selectFavoriteHotel(params);
     }
+
+	public List<HashMap<String, String>> selectPlanList(Member user) {
+		return dao.selectPlanList(user);
+	}
+
+	public int insertPlan(Plan plan) {
+		
+		return dao.insertPlan(plan);
+	}
+
+	public int deletePlan(Plan plan) {
+		return dao.deletePlan(plan);
+	}
 }
